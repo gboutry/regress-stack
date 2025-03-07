gboutry/regress-stack

commit 68aad51e3732a4d8c49b75fac6da65edc4d12ef2

Use ubuntu image to ensure multi-platform
Cirros does not support s390x, use Ubuntu to perform functional tests.
Since ubuntu requires a bigger memory footprint, run tests serially not
to have multiple VMs launched at the same time.

Signed-off-by: Guillaume Boutry <guillaume.boutry@canonical.com>

src/regress_stack/__main__.py
@@ -9,6 +9,7 @@ import regress_stack.modules
 from regress_stack.core import utils
 from regress_stack.core.modules import get_execution_order, modules
 from regress_stack.modules import keystone
+from regress_stack.modules import utils as module_utils
 
 LOG = logging.getLogger(__name__)
 
@@ -64,15 +65,29 @@ def collect_logs():
 def test():
     env = keystone.auth_env()
     dir_name = "mycloud01"
+    release = utils.release()
     utils.run("tempest", ["init", dir_name])
     utils.run(
         "discover-tempest-config",
-        ["--create"],
+        [
+            "--create",
+            "--flavor-min-mem",
+            "1024",
+            "--flavor-min-disk",
+            "5",
+            "--image",
+            f"http://cloud-images.ubuntu.com/{release}/current/{release}-server-cloudimg-{utils.machine()}.img",
+        ],
         env=env,
         cwd=dir_name,
     )
+    module_utils.cfg_set(
+        dir_name + "/etc/tempest.conf",
+        ("validation", "image_ssh_user", "ubuntu"),
+        ("validation", "image_alt_ssh_user", "ubuntu"),
+    )
     try:
-        utils.run("tempest", ["run", "--smoke"], env=env, cwd=dir_name)
+        utils.run("tempest", ["run", "--smoke", "--serial"], env=env, cwd=dir_name)
     except subprocess.CalledProcessError:
         # silence to fail on the next command
         pass

src/regress_stack/core/utils.py
@@ -3,6 +3,7 @@ import functools
 import ipaddress
 import logging
 import pathlib
+import platform
 import socket
 import subprocess
 import time
@@ -144,3 +145,24 @@ def exists_cache(path: pathlib.Path):
         return wrapper
 
     return decorator
+
+
+def machine() -> str:
+    """Return machine type."""
+    machine_name = platform.machine().lower()
+    if machine_name == "x86_64":
+        return "amd64"
+    elif machine_name == "aarch64":
+        return "arm64"
+    elif machine_name == "powerpc":
+        return "ppc64el"
+    return machine_name
+
+
+def release() -> str:
+    """Return release name."""
+    try:
+        return platform.freedesktop_os_release()["VERSION_CODENAME"]
+    except Exception:
+        LOG.exception("Failed to get release name")
+        return "noble"

commit f1fe77cee9e07877078edf4a05825e5ca96c38a5

main: Introduce test concurrency argument.
The commit in the fixes tag hard coded tempest to run tests
serially.  While this may be required in constrained
environments, it prolongs execution unnecessarily when more
resources are available.

Introduce a --concurrency argument to the test sub-command which
defaults to 1.  When providing the 'auto' keyword, concurrency is
set to the rounded up value of number of CPUs divided by 3.

Fixes: 68aad51e3732 ("Use ubuntu image to ensure multi-platform")
Signed-off-by: Frode Nordahl <fnordahl@ubuntu.com>

src/regress_stack/__main__.py
@@ -66,7 +66,7 @@ def collect_logs():
 
 
 @utils.measure_time
-def test():
+def test(concurrency: int):
     env = keystone.auth_env()
     dir_name = "mycloud01"
     release = utils.release()
@@ -128,7 +128,13 @@ def test():
     try:
         utils.run(
             "tempest",
-            ["run", "--load-list", str(regress_list.relative_to(dir_name)), "--serial"],
+            [
+                "run",
+                "--load-list",
+                str(regress_list.relative_to(dir_name)),
+                "--concurrency",
+                concurrency,
+            ],
             env=env,
             cwd=dir_name,
         )
@@ -165,7 +171,15 @@ def main():
     parser_setup = subparsers.add_parser("setup", help="Execute the tests.")
     add_common_arguments(parser_setup)
 
-    subparsers.add_parser("test", help="Run the tests.")
+    parser_test = subparsers.add_parser("test", help="Run the tests.")
+    parser_test.add_argument(
+        "--concurrency",
+        nargs="?",
+        type=utils.concurrency_cb,
+        help=(
+            "The number of workers to use, defaults to 1.  The value auto sets concurrency to number of cpus / 3."
+        ),
+    )
 
     subparsers.add_parser("list-modules", help="List available modules.")
 
@@ -178,7 +192,7 @@ def main():
     elif args.command == "setup":
         setup(args.target)
     elif args.command == "test":
-        test()
+        test(concurrency=args.concurrency or 1)
     elif args.command == "list-modules":
         list_modules()
 

src/regress_stack/core/utils.py
@@ -5,6 +5,8 @@ import contextlib
 import functools
 import ipaddress
 import logging
+import math
+import multiprocessing
 import pathlib
 import platform
 import socket
@@ -185,3 +187,13 @@ def is_setup_done(name: str) -> bool:
     """Check if task is done."""
     done_file = REGRESS_STACK_DIR / (name + ".setup")
     return done_file.exists()
+
+
+def concurrency_cb(arg: str) -> int:
+    """Handle concurrency argument, for use with ArgumentParser.
+
+    :raises: ValueError.
+    """
+    if arg == "auto":
+        return math.ceil(multiprocessing.cpu_count() / 3)
+    return int(arg)

tests/core/test_utils.py
@@ -1,0 +1,22 @@
+from unittest.mock import Mock
+
+import pytest
+
+import regress_stack.core.utils
+
+
+@pytest.fixture
+def mock_cpu_count(monkeypatch):
+    cpu_count = Mock(return_value=42 * 3)
+
+    monkeypatch.setattr("regress_stack.core.utils.multiprocessing.cpu_count", cpu_count)
+    yield cpu_count
+
+
+def test_concurrency_cb(mock_cpu_count):
+    assert type(regress_stack.core.utils.concurrency_cb("auto")) is int
+    assert regress_stack.core.utils.concurrency_cb("auto") == 42
+    assert type(regress_stack.core.utils.concurrency_cb("51")) is int
+    assert regress_stack.core.utils.concurrency_cb("51") == 51
+    with pytest.raises(ValueError):
+        regress_stack.core.utils.concurrency_cb("NotInt")